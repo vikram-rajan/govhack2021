f502c1e2ecdde0a3b3663a4f661981d0adb92d1e
Vikram Rajan <vikram-rajan@live.com.au>
2021-08-21 16:27:16 +1000

    australia map first cut

---
+++ react-app/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MyMap from "./components/MyMap";
+
+function App() {
+  return <MyMap />;
+}
+
+export default App;
---
+++ react-app/src/components/MyMap.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Map, GeoJSON } from "react-leaflet";
+import mapData from "./../data/australia.json";
+import "leaflet/dist/leaflet.css";
+import "./MyMap.css";
+
+class MyMap extends Component {
+  state = { color: "#ffff00" };
+
+  colors = ["green", "blue", "yellow", "orange", "grey"];
+
+  componentDidMount() {
+    console.log(mapData);
+  }
+
+  countryStyle = {
+    fillColor: "red",
+    fillOpacity: 1,
+    color: "black",
+    weight: 2,
+  };
+
+  printMesssageToConsole = (event) => {
+    console.log("Clicked");
+  };
+
+  changeCountryColor = (event) => {
+    event.target.setStyle({
+      color: "green",
+      fillColor: this.state.color,
+      fillOpacity: 1,
+    });
+  };
+
+  onEachCountry = (country, layer) => {
+    const countryName = country.properties.st_ply_pid;
+    console.log(countryName);
+    layer.bindPopup(countryName);
+
+    layer.options.fillOpacity = Math.random();
+
+    layer.on({
+      click: this.changeCountryColor,
+    });
+  };
+
+  colorChange = (event) => {
+    this.setState({ color: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>NSW</h1>
+        <Map style={{ height: "80vh" }} zoom={2} center={[33.8688, 151.2093]}>
+          <GeoJSON
+            style={this.countryStyle}
+            data={mapData.features}
+          />
+        </Map>
+        <input
+          type="color"
+          value={this.state.color}
+          onChange={this.colorChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyMap;
